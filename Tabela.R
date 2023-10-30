305fe6563489899cad1f94c5c5f378676ea4cf1a
julia-nosralla <nosralla.julia@gmail.com>
2023-10-30 20:23:45 -0300

    .

--- Tabela.R
+++ Tabela.R
@@ -5,7 +5,6 @@ tabela <- Dados1 %>%
   filter (! duplicated(`Product ID`)) %>%
   filter(Category != "NA") %>%
   filter(Price != "NA") %>%
-  mutate(Data.Venda = mdy(Data.Venda))
   filter(`Data Venda`!= "NA") %>%
   mutate(`Data Venda` = mdy(`Data Venda`))
 
